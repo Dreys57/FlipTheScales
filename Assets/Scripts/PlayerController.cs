1c1df80edce861570ac6ab02ef38e325009896b8
Vincent <skriosdreys@gmail.com>
2020-01-17 13:06:23 +0100

    Adding Unity Project and base controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D body;
+    
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        body.velocity = new Vector2(5f, body.velocity.y);
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (body.gravityScale > 0)
+            {
+                body.gravityScale -= 8;
+            }
+            else
+            {
+                body.gravityScale += 8;
+            }
+        }
+    }
+}
