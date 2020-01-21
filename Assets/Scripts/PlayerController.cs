9ff28ff4e219bf039fd7d8597836ffc571512494
Vincent <skriosdreys@gmail.com>
2020-01-21 11:48:54 +0100

    Tweaked the camera settings and wrote respawn script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D body;
+
+    private Vector3 startPos;
     
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+
+        startPos = transform.position;
     }
 
     void FixedUpdate()
@@ -28,6 +33,14 @@ public class PlayerController : MonoBehaviour
             {
                 body.gravityScale += 8;
             }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Spikes"))
+        {
+            transform.position = startPos;
         }
     }
 }
